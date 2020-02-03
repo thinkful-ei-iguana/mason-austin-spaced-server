96a2330575b4f41cee276abccad89cb955129c47
AustinT33 <austintumlinson@gmail.com>
2020-02-03 10:36:03 -0600

    tests passing

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,7 +6,6 @@ module.exports = {
   "host": process.env.MIGRATION_DB_HOST,
   "port": process.env.MIGRATION_DB_PORT,
   "database": process.env.MIGRATION_DB_NAME,
-  "testdatabase": process.env.MIGRATION_TEST_DB_NAME,
   "username": process.env.MIGRATION_DB_USER,
   "password": process.env.MIGRATION_DB_PASS
 }
