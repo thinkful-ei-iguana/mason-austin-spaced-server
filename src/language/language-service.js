129643d3d8caa65f26a41c82491ec1c865c002d6
AustinT33 <austintumlinson@gmail.com>
2020-02-04 10:50:47 -0600

    lang services

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,3 +1,5 @@
+const LinkedList = require('./LinkedList')
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
@@ -28,6 +30,51 @@ const LanguageService = {
       )
       .where({ language_id })
   },
+
+  updateLanguageWords(db, list) {
+    return db.transaction(async trx => {
+      let currentNode = list.headId
+      
+      while (currentNode !== null) {
+        await trx
+          .from('word')
+          .where('id', currentNode.word.id)
+          .update({
+            memory_value: currentNode.word.memory_value,
+            correct_count: currentNode.word.correct_count,
+            incorrect_count: currentNode.word.incorrect_count,
+            next: currentNode.word.next
+          })
+          currentNode = currentNode.next
+      }
+    })
+  },
+
+  getLanguageHead(db, id) {
+    return db
+      .from('language')
+      .where('language.id', id)
+      .join('word', 'language.head', '=', 'word.id')
+      .select(
+        'word.original',
+        'word.translation',
+        'word.correct_count',
+        'word.inccorect_count',
+        'language.total_score'
+      )
+      .first()
+  },
+
+  updateLanguageHead(db, langId, newHeadId, updatedTotalScore) {
+    return db
+      .from('language')
+      .where('id', langId)
+      .update({
+        head: newHeadId,
+        total_score: updatedTotalScore
+      })
+  },
+
   getLinkedList(words, headId) {
     const LL = new LinkedList()
 
@@ -44,6 +91,25 @@ const LanguageService = {
 
     return LL
   },
+
+  updateLinkedList(list, correct) {
+    const { word } = list.head
+    if (correct) {
+      word.correct_count += 1
+      word.memory_value *= 2
+    } else {
+      word.incorrect_count += 1
+      word.memory_value = 1
+    }
+
+    list.shiftHead(word.memory_value)
+
+    return {
+      wordCorrectCount: word.correct_count,
+      wordIncorrectCount: word.incorrect_count,
+      answer: word.translation
+    }
+  }
 }
 
 module.exports = LanguageService
