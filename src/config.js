630237d1214027425e82a705af2c00c089f5d5e9
AustinT33 <austintumlinson@gmail.com>
2020-02-05 11:10:38 -0600

    deploying

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL || "postgresql://vapor:123@localhost/spaced",
-  TEST_DATABASE_URL: process.env.TEST_DB_URL || "postgresql://vapor:123@localhost/spaced_test",
+  DATABASE_URL: process.env.DATABASE_URL || "postgresql://vapor:123@localhost/spaced",
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://vapor:123@localhost/spaced_test",
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
